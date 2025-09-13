20ffacd0891db3d4b28c8c5bfcee3d87a2fc5ff6
Chinmay-innovates <chinmaygope69@gmail.com>
2025-09-14 04:51:15 +0530

    setup logger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,29 @@
+import logger from '#config/logger.ts';
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(cookieParser());
+app.use(helmet());
+app.use(
+  morgan('combined', {
+    stream: {
+      write: (message) => {
+        logger.info(message.trim());
+      },
+    },
+  })
+);
+
 app.get('/', (req, res) => {
+  logger.info('Hello World!');
   res.status(200).send('Hello World!');
 });
 
