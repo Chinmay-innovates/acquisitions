e6dd41cc7c7843e0a0e5e9974c99ecca4d6f47f2
Chinmay-innovates <chinmaygope69@gmail.com>
2025-09-14 04:30:21 +0530

    setup Neon Postgres w/ Drizzle

---
+++ src/models/user.model.ts
@@ -0,0 +1,11 @@
+import { pgTable, serial, text, timestamp, varchar } from 'drizzle-orm/pg-core';
+
+export const user = pgTable('users', {
+  id: serial('id').primaryKey(),
+  name: varchar('name', { length: 256 }).notNull(),
+  email: varchar('email', { length: 256 }).notNull(),
+  password: varchar('password', { length: 256 }).notNull(),
+  role: varchar('role', { length: 50 }).notNull().default('user'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
